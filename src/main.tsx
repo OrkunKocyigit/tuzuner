f8c9934155206cdc5cd138bbf590b16f622ca439
Orkun Koçyiğit <orkunkocyigit@gmx.com>
2023-12-29 17:14:13 +0300

    fix: Router moved to App.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
 import { CSSVariablesResolver, MantineProvider } from "@mantine/core";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./router/router.tsx";
 import "./i18n/i18n.ts";
+import App from "./App.tsx";
 
 const resolver: CSSVariablesResolver = (theme) => ({
   variables: {},
@@ -18,9 +16,7 @@ const resolver: CSSVariablesResolver = (theme) => ({
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <MantineProvider forceColorScheme="dark" cssVariablesResolver={resolver}>
-      <RouterProvider router={router}>
-        <App />
-      </RouterProvider>
+      <App></App>
     </MantineProvider>
   </React.StrictMode>,
 );
